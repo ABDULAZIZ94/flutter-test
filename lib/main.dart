ed056d676a98fd31e2e86bc5eee09052a56c95f1
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-15 14:23:57 +0800

    container basic ok

--- lib/main.dart
+++ lib/main.dart
@@ -18,50 +18,18 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
    MyHomePage({Key key, this.title}) : super(key: key);
    final String title;
-
    @override
    Widget build(BuildContext context) {
-      return Scaffold(
-         appBar: AppBar(
-            title: Text(this.title),
-         ),
-         body: Center(
-            child:
-            MyButton()
-         ),
-      );
-   }
-}
-class MyButton extends StatelessWidget {
-   MyButton({Key key}) : super(key: key); 
-
-   @override 
-   Widget build(BuildContext context) {
       return Container(
-         decoration: const BoxDecoration(
-            border: Border(
-               top: BorderSide(width: 1.0, color: Color(0xFFFFFFFFFF)),
-               left: BorderSide(width: 1.0, color: Color(0xFFFFFFFFFF)),
-               right: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
-               bottom: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
-            ),
-         ),
-         child: Container(
-            padding: const
-            EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
-            decoration: const BoxDecoration(
-               border: Border(
-                  top: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
-                  left: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
-                  right: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
-                  bottom: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
+         decoration: BoxDecoration(color: Colors.white,),
+         padding: EdgeInsets.all(25), child: Center(
+            child:Text(
+               'Hello World', style: TextStyle(
+                  color: Colors.black, letterSpacing: 0.5, fontSize: 20,
                ),
-               color: Colors.grey,
+               textDirection: TextDirection.ltr,
             ),
-            child: const Text(
-               'OK',textAlign: TextAlign.center, style: TextStyle(color: Colors.black)
-            ), 
-         ), 
-      ); 
+         )
+      );
    }
 }
