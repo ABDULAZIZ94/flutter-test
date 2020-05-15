45ef261e4cb2426bd790db05338d5d39028a0fb2
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-15 14:30:21 +0800

    advanced layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,26 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart'; 
+void main() => runApp(MyApp()); 
 
 class MyApp extends StatelessWidget {
    // This widget is the root of your application.
-   @override
+   @override 
    Widget build(BuildContext context) {
-      return MaterialApp(
-         title: 'Hello World Demo Application',
-         theme: ThemeData(
-            primarySwatch: Colors.blue,
-         ),
-         home: MyHomePage(title: 'Home page'),
-      );
-   }
-}
+      return MaterialApp( 
+         title: 'Flutter Demo', theme: ThemeData( 
+         primarySwatch: Colors.blue,), 
+         home: MyHomePage(title: 'Product layout demo home page'),
+      ); 
+   } 
+} 
 class MyHomePage extends StatelessWidget {
-   MyHomePage({Key key, this.title}) : super(key: key);
-   final String title;
-   @override
+   MyHomePage({Key key, this.title}) : super(key: key); 
+   final String title; 
+      
+   @override 
    Widget build(BuildContext context) {
-      return Container(
-         decoration: BoxDecoration(color: Colors.white,),
-         padding: EdgeInsets.all(25), child: Center(
-            child:Text(
-               'Hello World', style: TextStyle(
-                  color: Colors.black, letterSpacing: 0.5, fontSize: 20,
-               ),
-               textDirection: TextDirection.ltr,
-            ),
-         )
-      );
+      return Scaffold(
+         appBar: AppBar(title: Text(this.title),), 
+         body: Center(child: Text( 'Hello World', )), 
+      ); 
    }
 }
